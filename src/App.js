7704d76823dbe152e68ee7dc435e2e9d5378d21f
shivraj0002 <95290113+shivraj0002@users.noreply.github.com>
2022-11-22 22:02:51 +0530

    fixed heroku issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import LandingP  from "./components/LandingP";
+import LandingP from "./components/LandingP";
 import ErrorPage from "./pages/ErrorPage";
 import FillPersonalData from "./components/forms/FillPersonalData";
 import FillEducationalData from "./components/forms/FillEducationalData";
@@ -15,42 +15,41 @@ import Navbar from "./components/Navbar";
 import ResumeActions from "./components/ResumeActions";
 import ReactStatusAlert from "./components/ReactStatusAlert";
 function App() {
-  const [user , setUser] = useState({})
+  const [user, setUser] = useState({})
   const dispatch = useDispatch()
   useEffect(() => {
-    
-     onAuthStateChanged(auth , (currentUser)=>{
-           setUser(currentUser)
-           console.log('in app js useeffect')
-           if(currentUser?.email)
-           {
-             dispatch(userAuthActions.userLoginStatus(true))
-             dispatch(userAuthActions.setUserId(user?.uid))
-             dispatch(userAuthActions.setUserEmail(user?.email))
-           }
-     })
-    
-  }, [user ,dispatch])
+
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser)
+      console.log('in app js useeffect')
+      if (currentUser?.email) {
+        dispatch(userAuthActions.userLoginStatus(true))
+        dispatch(userAuthActions.setUserId(user?.uid))
+        dispatch(userAuthActions.setUserEmail(user?.email))
+      }
+    })
+
+  }, [user, dispatch])
   //  const handleThis = () =>{
   //   console.log(user?.email)
   //   console.log(user?.uid)
   //  }
 
   return (
-   <>
-     <Navbar />
-     <ReactStatusAlert />
-     <Routes>
-      <Route exact path="/" element={<Navigate to={'/home'} />} />
-      <Route path="/resume-actions"   element={<ResumeActions />} />
-      <Route path="/home"   element={<LandingP />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/form-personal" element={<FillPersonalData />} />
-      <Route path="/form-educational" element={<FillEducationalData />} />
-      <Route path="/*" element={<ErrorPage />} />
-     </Routes>
-   </>
+    <>
+      <Navbar />
+      <ReactStatusAlert />
+      <Routes>
+        <Route exact path="/" element={<LandingP />} />
+        <Route path="/resume-actions" element={<ResumeActions />} />
+        <Route path="/home" element={<LandingP />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/form-personal" element={<FillPersonalData />} />
+        <Route path="/form-educational" element={<FillEducationalData />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+    </>
   );
 }
 
